1b03cbd7a754904cd4120f587217a782d1a00f46
Zkhan601 <95313563+Zkhan601@users.noreply.github.com>
2022-11-18 13:05:07 -0500

    Update

---
+++ Audio/audio.js
@@ -0,0 +1,29 @@
+const birds = document.getElementById("birds");
+const playBirds = () => {
+    console.log("play birds");
+    birds.play();
+};
+
+const doorbell = document.getElementById("doorbell");
+const playDoorbell = () => {
+    console.log("play doorbell");
+    doorbell.play();
+};
+
+const helicopter= document.getElementById("helicopter");
+const playHelicopter = () => {
+    console.log("play helicopter");
+    helicopter.play();
+};
+
+const motorcycle = document.getElementById("motorcycle");
+const playMotorcycle = () => {
+    console.log("play motorcycle");
+    motorcycle.play();
+};
+
+const rain = document.getElementById("rain");
+const playRain = () => {
+    console.log("play rain");
+    rain.play();
+};
